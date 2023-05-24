3daff77b58e35f74076c6bb48ec153d2ace0e242
Max Pike <mpike27@stanford.edu>
2023-05-24 18:28:35 -0400

    Initial commits by Max

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -7,6 +7,13 @@ class State(Enum):
     AWAITING_MESSAGE = auto()
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
+    USER_FIRST_PROMPT = auto()
+    USER_FRAUD = auto()
+    USER_VERBAL_ABUSE = auto()
+    USER_HARASSMENT = auto()
+    USER_SENSITIVE_CONTENT = auto()
+    USER_OTHER = auto()
+    USER_BLOCK = auto()
 
 class Report:
     START_KEYWORD = "report"
@@ -59,7 +66,70 @@ class Report:
                     "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
         
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+            self.state = State.USER_FIRST_PROMPT
+            return ["Why are you reporting this message (please respond with the corresponding number)? \n(1) Fraud\n(2) Verbal Abuse\n(3) Harassment/Intimidation of Violence\n(4) Sensitive/Disturbing Content\n(5) Other\n"]
+        
+        if self.state == State.USER_FIRST_PROMPT:
+            if message == '1':
+                self.state = State.USER_FRAUD
+                return ["Please select the type of harm (please respond with the corresponding number). \n(1) Impersonation\n(2) Scam\n(3) Solicitation\n"] 
+            if message == '2':
+                self.state = State.USER_VERBAL_ABUSE
+                reply = "Please select the type of harm (please respond with the corresponding number). \n"
+                reply += "(1) Violent Language: Celebration of violent acts\n"
+                reply += "(2) Violent Language: Violent event denial\n"
+                reply += "(3) Violent Language: Dehumanization\n"
+                reply += "(4) Violent Language: Inciting or encouraging violence\n"
+                reply += "(5) Hate Speech: Ethnicity/Race/Nationality\n"
+                reply += "(6) Hate Speech: Gender/Sex/Sexuality\n"
+                reply += "(7) Hate Speech: Religion\n"
+                reply += "(8) Hate Speech: Political Beliefs\n"
+                reply += "(9) Hate Speech: Disability/Health Status\n"
+                reply += "(10) Hate Speech: Other\n"
+                return [reply] 
+            if message == '3':
+                self.state = State.USER_HARASSMENT
+                return ["Please select the type of harm (please respond with the corresponding number). \n(1) Sexual Harassment\n(2) Threatening to post or posting private info\n(3) Stalking/Threats to injure\n"]  
+            if message == '4':
+                self.state = State.USER_SENSITIVE_CONTENT
+                return ["Please select the type of harm (please respond with the corresponding number). \n(1) Child Exploitation\n(2) Assault\n(3) Beastiality\n(4) Self Harm\n)"] 
+            if message == '5':
+                self.state = State.USER_OTHER
+                return ["Why is this message harmful?"] 
+        
+        if self.state == State.USER_FRAUD and message in ('1', '2', '3'):
+            reply = 'Thank you for your report. Our 24/7 moderation team will review it shortly.\n'
+            reply += 'Would you like to block this user to prevent future interactions with this user?\n'
+            self.state = State.USER_BLOCK
+            return reply
+        
+        if self.state == State.USER_VERBAL_ABUSE and message in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'):
+            reply = 'Thank you for your report. Our 24/7 moderation team will review it shortly.\n'
+            reply += 'Would you like to block this user to prevent future interactions with this user?\n'
+            self.state = State.USER_BLOCK
+            return reply
+        
+        if self.state == State.USER_HARASSMENT and message in ('1', '2', '3'):
+            reply = 'Thank you for your report. Our 24/7 moderation team will review it shortly.\n'
+            reply += 'Would you like to block this user to prevent future interactions with this user?\n'
+            self.state = State.USER_BLOCK
+            return reply
+        
+        if self.state == State.USER_SENSITIVE_CONTENT and message in ('1', '2', '3', '4'):
+            reply = 'Thank you for your report. Our 24/7 moderation team will review it shortly.\n'
+            reply += 'Would you like to block this user to prevent future interactions with this user?\n'
+            self.state = State.USER_BLOCK
+            return reply
+        
+        if self.state == State.USER_OTHER:
+            reply = 'Thank you for your report. Our 24/7 moderation team will review it shortly.\n'
+            reply += 'Would you like to block this user to prevent future interactions with this user (yes/no)?\n'
+            self.state = State.USER_BLOCK
+            return reply
+        
+        if self.state == State.USER_BLOCK and message in ('yes', 'no'):
+            self.report_complete()
+            return ['Done. Report Closed.']
 
         return []
 

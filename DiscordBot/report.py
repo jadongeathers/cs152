7b8b087d945910d05c5330ca63e1dac11a929a78
Max Pike <mpike27@stanford.edu>
2023-05-25 16:08:40 -0400

    First commit of Review class

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -136,6 +136,106 @@ class Report:
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
     
+class ReviewState(Enum):
+    REVIEW_START = auto()
+    REVIEW_FIRST_MESSAGE = auto()
+    REVIEW_ILLEGAL_OR_THREATENING = auto()
+    REVIEW_NOT_ILLEGAL_OR_THREATENING = auto()
+    REVIEW_FRAUD = auto()
+    REVIEW_HATE_SPEECH = auto()
+    REVIEW_OTHER = auto()
+    REVIEW_HARASSMENT = auto()
+    REVIEW_SENSITIVE = auto()
+    REVIEW_NOT_ILLEGAL = auto()
+
+class Review:
+    START_KEYWORD = "review"
+
+    def __init__(self, client):
+        self.state = ReviewState.REVIEW_START
+        self.client = client
+        self.message = None
+    
+    async def handle_message(self, message):
+        '''
+        This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what 
+        prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
+        get you started and give you a model for working with Discord. 
+        '''
+        
+        if self.state == ReviewState.REVIEW_START:
+            reply =  "Thank you for starting the review process. \n"
+            reply += 'Is the reported content illegal/immediately threatening to users? (yes/no)'
+            self.state = ReviewState.REVIEW_FIRST_MESSAGE
+            return [reply]
+        
+        if self.state == ReviewState.REVIEW_FIRST_MESSAGE and message in ('yes', 'no'):
+            if message == 'yes':
+                self.state = ReviewState.REVIEW_ILLEGAL_OR_THREATENING
+                return ['To which category does this report belong (please respond with the corresponding number): \n(1) Harassment/Intimidation of violence \n(2) Sensitive/Disturbing Content']
+            else:
+                self.state = ReviewState.REVIEW_NOT_ILLEGAL_OR_THREATENING
+                return ['To which category does this report belong (please respond with the corresponding number): \n(1) Fraud \n(2) Hate Speech \n(3) Other']
+        
+        if self.state == ReviewState.REVIEW_ILLEGAL_OR_THREATENING and message in ('1', '2'): 
+            if message == '1':
+                self.state = ReviewState.REVIEW_HARASSMENT
+                return ['Is the content illegal (yes/no)?'] 
+            else: 
+                self.state = ReviewState.REVIEW_SENSITIVE
+                return ['Which category does it fall under (please respond with the corresponding number): \n(1) CSAM\n (2) Self Harm \n(3) Other\n']  
+
+        if self.state == ReviewState.REVIEW_NOT_ILLEGAL_OR_THREATENING and message in ('1', '2', '3'): 
+            if message == '1':
+                self.state = ReviewState.REVIEW_FRAUD 
+                return ['Has the user previously received a disciplinary action (yes/no)?']
+            elif message == '2': 
+                self.state = ReviewState.REVIEW_HATE_SPEECH 
+                return ['Has the user previously received a disciplinary action (yes/no)?'] 
+            else:
+                self.state = ReviewState.REVIEW_OTHER
+                return ['Has the user previously received a disciplinary action (yes/no)?'] 
+        
+        if self.state in (ReviewState.REVIEW_FRAUD, ReviewState.REVIEW_HATE_SPEECH, ReviewState.REVIEW_OTHER) and message in ('yes', 'no'):
+            if message == 'yes':
+                self.review_complete(self)
+                return ['Take down post - move reported user one step up the disciplinary actions hierarchy']
+            else:
+                self.review_complete(self)
+                return ['Take down post - send user a warning']
+            
+        if self.state == ReviewState.REVIEW_SENSITIVE and message in ('1', '2', '3'):
+            if message == '1':
+                self.review_complete()
+                return ['Report to NCMEC + Ban user + store content securely as required by law']
+            if message == '2':
+                self.review_complete()
+                return ['Connect reported user with self help resources']
+            if message == '3':
+                self.state = ReviewState.REVIEW_HARASSMENT
+                return ['Is the content illegal (yes/no)?'] 
+        
+        if self.state == ReviewState.REVIEW_HARASSMENT and message in ('yes', 'no'):
+            if message == 'yes':
+                self.review_complete()
+                return ['Ban User + Store content securely as required by law']
+            else: 
+                self.state = ReviewState.REVIEW_NOT_ILLEGAL
+                return ['Has the reported user previously received a one week feature block (yes/no)?']
+            
+        if self.state == ReviewState.REVIEW_NOT_ILLEGAL and message in ('yes','no'):
+            if message == 'yes':
+                self.review_complete()
+                return ['One week mute/suspension + warning']
+            else: 
+                self.review_complete()
+                return ['Ban User']
+        
+        return []
+
+    def review_complete(self):
+        return self.state == State.REPORT_COMPLETE
+    
 
 
     

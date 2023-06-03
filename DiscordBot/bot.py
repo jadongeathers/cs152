fc6a12ef1ff5527389e3f64ff3f3c8cad9fe90f7
Jadon Geathers <jadongeathers@DN0a226888.SUNet>
2023-06-03 16:55:14 -0700

    Updated bot to make automatic reports; implemented Google Perspective classification

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -12,6 +12,14 @@ from report import Report, Review
 import pdb
 from data_manager import DataManager
 
+# Thresholds for classification
+COEFFS = {
+    'intercept': -1.3228379330359248,
+    'INSULT': 3.8921009196729774,
+    'IDENTITY_ATTACK': 0.9563236343157535,
+    'THREAT': 0.31681785782996336
+}
+
 # Set up logging to the console
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -29,6 +37,7 @@ with open(token_path) as f:
     discord_token = tokens['discord']
     google_token = tokens['google']
 
+# Google Perspective API setup
 API_KEY = google_token
 
 google = discovery.build(
@@ -51,9 +60,10 @@ class ModBot(discord.Client):
         self.group_num = None
         self.mod_channels = {} # Map from guild to the mod channel id for that guild
         self.reports = {} # Map from user IDs to the state of their report
-        self.unreviewed = {} # Map from user IDs to unreview report
+        self.unreviewed = {} # Map from user IDs to unreviewed reports
         self.reviews = {} # Map from user IDs to the state of their review
         self.data_manager = DataManager()
+        self.report_in_progress = False
 
     async def on_ready(self):
         print(f'{self.user.name} has connected to Discord! It is these guilds:')
@@ -93,7 +103,7 @@ class ModBot(discord.Client):
     async def handle_dm(self, message):
         # Handle a help message
         if message.content == Report.HELP_KEYWORD:
-            reply =  "Use the `report` command to begin the ing process.\n"
+            reply =  "Use the `report` command to begin the reporting process.\n"
             reply += "Use the `cancel` command to cancel the report process.\n"
             await message.channel.send(reply)
             return
@@ -108,7 +118,9 @@ class ModBot(discord.Client):
         # If we don't currently have an active report for this user, add one
         if author_id not in self.reports:
             self.reports[author_id] = Report(self)
-            self.unreviewed[author_id] = self.reports[author_id]
+            self.unreviewed[author_id] = [self.reports[author_id]]
+        else:
+            self.unreviewed[author_id].append(Report(self))
 
         # Let the report class handle this message; forward all the messages it returns to uss
         responses = await self.reports[author_id].handle_message(message)
@@ -147,49 +159,74 @@ class ModBot(discord.Client):
             responses = []
 
             # If there are no reports, return
-            if not self.unreviewed:
+            if not self.unreviewed and message.content == Review.START_KEYWORD:
                 await message.channel.send('No available unreviewed reports or reports in progress.')
                 return
 
             # Select a report (no priority yet)
-            user_report_id = list(self.unreviewed.keys())[0]
-            report = self.unreviewed[user_report_id]
-            offending_message = report.offending_message
-            reporter = report.reporter_id
+            # user_report_id = list(self.unreviewed.keys())[0]
+            # report = self.unreviewed[user_report_id][0]
+            # offending_message = report.offending_message
+            # reporter = report.reporter_id
+
+            user_report_id = None
 
             # Only respond to messages if they're part of a review flow
             if author_id not in self.reviews and not message.content.startswith(Review.START_KEYWORD):
                 return
 
             # If we don't currently have an active review for this mod, add one
-            if author_id not in self.reviews:
-                await message.channel.send('Below is the reported content:')
-                await message.channel.send("```" + offending_message.author.name + ": " + offending_message.content + "```")
-                await message.channel.send('Reporting User ' + str(reporter) + ' has made ' + str(self.data_manager.get_reports_confirmed(reporter))
-                                           + ' previous reports with ' + str(self.data_manager.get_trust_score(reporter)) + '% accuracy ')
-                self.reviews[author_id] = Review(self)
+            if author_id not in self.reviews and message.content == Review.START_KEYWORD:
+                self.report_in_progress = True
+                self.reviews[author_id] = Review(self, unreviewed=self.unreviewed, data_manager=self.data_manager)
 
-            # Let the review class handle this message; forward all the messages it returns to us
-            responses = await self.reviews[author_id].handle_message(message)
-            for r in responses:
-                await message.channel.send(r)
+            if self.report_in_progress:
+                # Let the review class handle this message; forward all the messages it returns to us
+                responses = await self.reviews[author_id].handle_message(message)
+                for r in responses:
+                    await message.channel.send(r)
 
-            # If the review is complete or cancelled, remove it from the appropriate maps
-            if self.reviews[author_id].review_complete():
-                await message.channel.send('Done. Review complete.')
-                # if report is accurate, incrament accurate reports count by one
-                if not self.reviews[author_id].noaction:
-                    self.data_manager.add_true_report(user_report_id)
-                # if report isnt cancelled, increment confirmed reports by one
-                if not self.reviews[author_id].review_cancelled():
-                    self.data_manager.add_confirmed_report(user_report_id)
-                self.reviews.pop(author_id)
-                self.unreviewed.pop(user_report_id)
-            if self.reviews and self.reviews[author_id].review_cancelled():
-                self.reviews.pop(author_id)
+                if author_id in self.reviews and self.reviews[author_id].review_cancelled():
+                    self.reviews.pop(author_id)
+                    self.report_in_progress = False
+
+                # If there are no reports for this category, remove the review
+                if author_id in self.reviews and self.reviews[author_id].cannot_review():
+                    self.reviews.pop(author_id)
+                    self.report_in_progress = False
+
+                # If the review is complete or cancelled, remove it from the appropriate maps
+                if author_id in self.reviews and self.reviews[author_id].review_complete():
+                    user_report_id = self.reviews[author_id].user_report_id
+                    await message.channel.send('Done. Review complete.')
+                    # if report is accurate, increment accurate reports count by one
+                    if not self.reviews[author_id].noaction:
+                        self.data_manager.add_true_report(user_report_id)
+                    # if report isn't cancelled, increment confirmed reports by one
+                    if not self.reviews[author_id].review_cancelled():
+                        self.data_manager.add_confirmed_report(user_report_id)
+                    self.reviews.pop(author_id)
+                    self.unreviewed[user_report_id].pop(0)
+                    if not self.unreviewed[user_report_id]:
+                        self.unreviewed.pop(user_report_id)
+                    self.report_in_progress = False
 
         if message.channel.name == f'group-{self.group_num}':
-            score = self.eval_text(message)
+            scores = self.eval_text(message)
+            score = 0
+            for key in scores:
+                score += COEFFS[key] * scores[key]
+            score += COEFFS['intercept']
+
+            if score > 0.5:
+                await mod_channel.send('Made automatic report.')
+                report = Report(self)
+                report.reporter_id = 'BOT'
+                report.offending_message = message
+                self.data_manager.add_user_report('BOT')
+                if 'BOT' not in self.unreviewed:
+                    self.unreviewed['BOT'] = []
+                self.unreviewed['BOT'].append(report)
             await mod_channel.send(self.code_format(score))
 
         return

ed8e91e432a7b52b8239222f81e607accbdfe33d
Jadon Geathers <jadongeathers@DN0a226888.SUNet>
2023-06-03 16:55:42 -0700

    Fixed None error in DataManager

--- DiscordBot/data_manager.py
+++ DiscordBot/data_manager.py
@@ -6,13 +6,13 @@ class ReportInfo:
 
     def get_percentage(self):
         if self.total_reports_confirmed == 0:
-            return None
+            return 0
         return 100 * self.accurate_reports / self.total_reports_confirmed
 
 
 class DataManager:
     def __init__(self):
-        # creates dictionary wheere userids mad to ReportInfo structs
+        # creates dictionary where user ids mad to ReportInfo structs
         self.user_report_info = dict()
 
     def get_trust_score(self, user):
@@ -24,11 +24,11 @@ class DataManager:
             self.user_report_info[user] = ReportInfo()
         self.user_report_info[user].total_reports_filed += 1
 
-    # returns number of reports a user has made than have been reviewd by mod team
+    # returns number of reports a user has made than have been reviewed by mod team
     def get_reports_confirmed(self, user):
         return self.user_report_info[user].total_reports_confirmed
 
-    # when mod team has revirew a user's report, statistic is kept for that user
+    # when mod team has reviewed a user's report, statistic is kept for that user
     def add_confirmed_report(self, user):
         self.user_report_info[user].total_reports_confirmed += 1
 

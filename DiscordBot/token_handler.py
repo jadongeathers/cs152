0731949542edd77b043641dc72ac80f501fd22b4
AaronLi9 <aaronli9@stanford.edu>
2023-06-06 05:15:31 -0700

    added token handler file

---
+++ DiscordBot/token_handler.py
@@ -0,0 +1,24 @@
+import openai
+import json
+import os
+
+
+def handle_tokens(model):
+    token_path = "tokens.json"
+    if not os.path.isfile(token_path):
+        raise Exception(f"{token_path} not found!")
+    with open(token_path) as f:
+        # If you get an error here, it means your token is formatted incorrectly. Did you put it in quotes?
+        tokens = json.load(f)
+        if model == "google":
+            google_token = tokens["google"]
+            return google_token
+        elif model == "open_ai":
+            openai_token = tokens["open_ai"]
+            return openai_token
+        elif model == "chat_completion":
+            openai_organization = tokens["openai_organization"]
+            openai_token = tokens["open_ai"]
+            return openai_organization, openai_token
+        elif model == "combo":
+            return
